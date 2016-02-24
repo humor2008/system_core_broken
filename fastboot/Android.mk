DarkNess-reDefined/android_system_core

Fix build break for multiple definition
Remove unneeded library linked

Change-Id: Ice26d83a73c59460d717340ca57ee898d002dc54

fastboot/Android.mk
@@ -75,7 +75,7 @@ LOCAL_LDFLAGS_linux := -ldl -rdynamic -Wl,-rpath,.
 LOCAL_REQUIRED_MODULES_linux := libf2fs_fmt_host_dyn
 # The following libf2fs_* are from system/extras/f2fs_utils,
 # and do not use code in external/f2fs-tools.
-LOCAL_STATIC_LIBRARIES_linux += libf2fs_utils_host libf2fs_ioutils_host libf2fs_dlutils_host
+LOCAL_STATIC_LIBRARIES_linux += libf2fs_utils_host libf2fs_dlutils_host
 
 LOCAL_CXX_STL := libc++_static
 

humor2008/system_core_broken

fastboot/Android.mk
@@ -75,7 +75,7 @@ LOCAL_LDFLAGS += -ldl -rdynamic -Wl,-rpath,.
 LOCAL_REQUIRED_MODULES := libf2fs_fmt_host_dyn
 # The following libf2fs_* are from system/extras/f2fs_utils,
 # and do not use code in external/f2fs-tools.
-LOCAL_STATIC_LIBRARIES += libf2fs_utils_host libf2fs_ioutils_host libf2fs_dlutils_host
+LOCAL_STATIC_LIBRARIES += libf2fs_utils_host libf2fs_dlutils_host
 endif
 
 # libc++ not available on windows yet